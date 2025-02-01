a815071ad13e6ba1bef634d3d918a2d03f7487a4
dzaviryukha <d.zaviryukha@astoundcommerce.com>
2025-02-01 22:01:58 +0200

    test change 1.5

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 console.log('hello app.js');
 
 const btn = document.createElement('button');
-btn.textContent = 'show log v1.5';
+btn.textContent = 'show log v1.6';
 app.append(btn);
 
 btn.addEventListener('click', function(e) {
